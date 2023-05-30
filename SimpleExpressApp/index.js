c54ad550416f9f141ae9eda1e9b29b80943b0052
CL-Ganesh <133627079+CL-Ganesh@users.noreply.github.com>
2023-05-30 12:56:39 +0530

    Update index.js

--- SimpleExpressApp/index.js
+++ SimpleExpressApp/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("test pipeline");
+  res.send("simple exprees pipeline");
 });
 
 app.get("/info", (req, res) => {
